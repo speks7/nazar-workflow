8f9e963022add9324ea1e5e00516db8cf5f80789
Sunim Acharya <sunim.54@gmail.com>
2018-05-05 21:43:19 +0545

    Retriving botlenecks for both distor and non-distort condition

--- 02_Image Classification/02a_Class Model/04_Understanding Inception/retrain.py
+++ 02_Image Classification/02a_Class Model/04_Understanding Inception/retrain.py
@@ -11,6 +11,7 @@ from tensorflow.python.util import compat
 import collections
 from tensorflow.python.framework import tensor_shape
 import numpy as np
+import random
 
 FLAGS = None
 
@@ -675,6 +676,131 @@ def add_evaluation_step(result_tensor, ground_truth_tensor):
     tf.summary.scalar('accuracy', evaluation_step)
     return evaluation_step, prediction
 
+
+def get_random_distorted_bottlenecks(
+    sess, image_lists, how_many, category, image_dir, input_jpeg_tensor,
+    distorted_image, resized_input_tensor, bottleneck_tensor):
+    """Retrieves bottleneck values for training images, after distortions.
+
+    If we're training with distortions like crops, scales, or flips, we have to
+    recalculate the full model for every image, and so we can't use cached
+    bottleneck values. Instead we find random images for the requested category,
+    run them through the distortion graph, and then the full graph to get the
+    bottleneck results for each.
+
+    Args:
+        sess: Current TensorFlow Session.
+        image_lists: Dictionary of training images for each label.
+        how_many: The integer number of bottleneck values to return.
+        category: Name string of which set of images to fetch - training, testing,
+        or validation.
+        image_dir: Root folder string of the subfolders containing the training
+        images.
+        input_jpeg_tensor: The input layer we feed the image data to.
+        distorted_image: The output node of the distortion graph.
+        resized_input_tensor: The input node of the recognition graph.
+        bottleneck_tensor: The bottleneck output layer of the CNN graph.
+
+    Returns:
+        List of bottleneck arrays and their corresponding ground truths.
+    """
+    class_count = len(image_lists.keys())
+    bottlenecks = []
+    ground_truths = []
+    for unused_i in range(how_many):
+        label_index = random.randrange(class_count)
+        label_name = list(image_lists.keys())[label_index]
+        image_index = random.randrange(MAX_NUM_IMAGES_PER_CLASS + 1)
+        image_path = get_image_path(image_lists, label_name, image_index, image_dir,
+                                    category)
+        if not gfile.Exists(image_path):
+            tf.logging.fatal('File does not exist %s', image_path)
+        jpeg_data = gfile.FastGFile(image_path, 'rb').read()
+        # Note that we materialize the distorted_image_data as a numpy array before
+        # sending running inference on the image. This involves 2 memory copies and
+        # might be optimized in other implementations.
+        distorted_image_data = sess.run(distorted_image,
+                                        {input_jpeg_tensor: jpeg_data})
+        bottleneck_values = sess.run(bottleneck_tensor,
+                                    {resized_input_tensor: distorted_image_data})
+        bottleneck_values = np.squeeze(bottleneck_values)
+        ground_truth = np.zeros(class_count, dtype=np.float32)
+        ground_truth[label_index] = 1.0
+        bottlenecks.append(bottleneck_values)
+        ground_truths.append(ground_truth)
+    return bottlenecks, ground_truths
+
+
+def get_random_cached_bottlenecks(sess, image_lists, how_many, category,
+                                  bottleneck_dir, image_dir, jpeg_data_tensor,
+                                  decoded_image_tensor, resized_input_tensor,
+                                  bottleneck_tensor, architecture):
+    """Retrieves bottleneck values for cached images.
+
+    If no distortions are being applied, this function can retrieve the cached
+    bottleneck values directly from disk for images. It picks a random set of
+    images from the specified category.
+
+    Args:
+        sess: Current TensorFlow Session.
+        image_lists: Dictionary of training images for each label.
+        how_many: If positive, a random sample of this size will be chosen.
+        If negative, all bottlenecks will be retrieved.
+        category: Name string of which set to pull from - training, testing, or
+        validation.
+        bottleneck_dir: Folder string holding cached files of bottleneck values.
+        image_dir: Root folder string of the subfolders containing the training
+        images.
+        jpeg_data_tensor: The layer to feed jpeg image data into.
+        decoded_image_tensor: The output of decoding and resizing the image.
+        resized_input_tensor: The input node of the recognition graph.
+        bottleneck_tensor: The bottleneck output layer of the CNN graph.
+        architecture: The name of the model architecture.
+
+    Returns:
+        List of bottleneck arrays, their corresponding ground truths, and the
+        relevant filenames.
+    """
+    class_count = len(image_lists.keys())
+    bottlenecks = []
+    ground_truths = []
+    filenames = []
+    if how_many >= 0:
+        # Retrieve a random sample of bottlenecks.
+        for unused_i in range(how_many):
+            label_index = random.randrange(class_count)
+            label_name = list(image_lists.keys())[label_index]
+            image_index = random.randrange(MAX_NUM_IMAGES_PER_CLASS + 1)
+            image_name = get_image_path(image_lists, label_name, image_index,
+                                        image_dir, category)
+            bottleneck = get_or_create_bottleneck(
+                sess, image_lists, label_name, image_index, image_dir, category,
+                bottleneck_dir, jpeg_data_tensor, decoded_image_tensor,
+                resized_input_tensor, bottleneck_tensor, architecture)
+            ground_truth = np.zeros(class_count, dtype=np.float32)
+            ground_truth[label_index] = 1.0
+            bottlenecks.append(bottleneck)
+            ground_truths.append(ground_truth)
+            filenames.append(image_name)
+    else:
+        # Retrieve all bottlenecks.
+        for label_index, label_name in enumerate(image_lists.keys()):
+            for image_index, image_name in enumerate(
+                image_lists[label_name][category]):
+                image_name = get_image_path(image_lists, label_name, image_index,
+                                            image_dir, category)
+                bottleneck = get_or_create_bottleneck(
+                    sess, image_lists, label_name, image_index, image_dir, category,
+                    bottleneck_dir, jpeg_data_tensor, decoded_image_tensor,
+                    resized_input_tensor, bottleneck_tensor, architecture)
+                ground_truth = np.zeros(class_count, dtype=np.float32)
+                ground_truth[label_index] = 1.0
+                bottlenecks.append(bottleneck)
+                ground_truths.append(ground_truth)
+                filenames.append(image_name)
+    return bottlenecks, ground_truths, filenames
+
+
 def main(_):
     # Needed to make sure the logging output is visible
     tf.logging.set_verbosity(tf.logging.INFO)
@@ -761,7 +887,19 @@ def main(_):
         for i in range(FLAGS.how_many_training_steps):
             # Get a batch of input bottleneck values, either calculated fresh every
             # time with distortions applied, or from the cache stored on disk.
-            
+            if do_distort_images:
+                (train_bottlenecks,
+                train_ground_truth) = get_random_distorted_bottlenecks(
+                    sess, image_lists, FLAGS.train_batch_size, 'training',
+                    FLAGS.image_dir, distorted_jpeg_data_tensor,
+                    distorted_image_tensor, resized_image_tensor, bottleneck_tensor)     
+            else:
+                (train_bottlenecks,
+                train_ground_truth, _) = get_random_cached_bottlenecks(
+                    sess, image_lists, FLAGS.train_batch_size, 'training',
+                    FLAGS.bottleneck_dir, FLAGS.image_dir, jpeg_data_tensor,
+                    decoded_image_tensor, resized_image_tensor, bottleneck_tensor,
+                    FLAGS.architecture)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

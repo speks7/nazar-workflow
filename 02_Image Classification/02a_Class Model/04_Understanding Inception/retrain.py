723d0cbe1b06269c4f57d073bac8c178c73d44b6
Sunim Acharya <sunim.54@gmail.com>
2018-05-05 20:10:14 +0545

    Create list for choosing test,train,validation images

--- 02_Image Classification/02a_Class Model/04_Understanding Inception/retrain.py
+++ 02_Image Classification/02a_Class Model/04_Understanding Inception/retrain.py
@@ -5,6 +5,18 @@ import os
 from six.moves import urllib
 import tarfile
 from tensorflow.python.platform import gfile
+import re
+import hashlib
+from tensorflow.python.util import compat
+import collections
+
+FLAGS = None
+
+# These are all parameters that are tied to the particular model architecture
+# we're using for Inception v3. These include things like tensor names and their
+# sizes. If you want to adapt this script to work with another model, you will
+# need to update these to reflect the values in the network you're using.
+MAX_NUM_IMAGES_PER_CLASS = 2 ** 27 - 1  # ~134M
 
 def ensure_dir_exists(dir_name):
     """Makes sure the folder exists on disk.
@@ -128,6 +140,92 @@ def create_model_graph(model_info):
                 ]))
     return graph, bottleneck_tensor, resized_input_tensor
 
+def create_image_lists(image_dir, testing_percentage, validation_percentage):
+    """Builds a list of training images from the file system.
+
+    Analyzes the sub folders in the image directory, splits them into stable
+    training, testing, and validation sets, and returns a data structure
+    describing the lists of images for each label and their paths.
+
+    Args:
+        image_dir: String path to a folder containing subfolders of images.
+        testing_percentage: Integer percentage of the images to reserve for tests.
+        validation_percentage: Integer percentage of images reserved for validation.
+
+    Returns:
+        A dictionary containing an entry for each label subfolder, with images split
+        into training, testing, and validation sets within each label.
+    """
+    if not gfile.Exists(image_dir): # If no img dir show error
+        tf.logging.error("Image directory '" + image_dir + "' not found.")
+        return None
+    result = collections.OrderedDict() #Setup ordered list
+    sub_dirs = [
+        os.path.join(image_dir,item)
+        for item in gfile.ListDirectory(image_dir)] #item are files inside a directory returned as list
+    sub_dirs = sorted(item for item in sub_dirs
+                        if gfile.IsDirectory(item)) #Check if the current item is directory
+    for sub_dir in sub_dirs: #For folders inside img dir
+        extensions = ['jpg', 'jpeg', 'JPG', 'JPEG']
+        file_list = []
+        dir_name = os.path.basename(sub_dir)
+        if dir_name == image_dir: #if the dir name is img dir continue loop
+            continue
+        tf.logging.info("Looking for images in '" + dir_name + "'")
+        for extension in extensions:
+            file_glob = os.path.join(image_dir, dir_name, '*.' + extension) #Join img_dir + sub_dir + *. + ext
+            file_list.extend(gfile.Glob(file_glob)) #return list of files
+        if not file_list:
+            tf.logging.warning('No files found') #no imgs in dir
+            continue
+        if len(file_list) < 20:
+            tf.logging.warning(
+                'WARNING: Folder has less than 20 images, which may cause issues.')
+        elif len(file_list) > MAX_NUM_IMAGES_PER_CLASS:
+            tf.logging.warning(
+                'WARNING: Folder {} has more than {} images. Some images will '
+                'never be selected.'.format(dir_name, MAX_NUM_IMAGES_PER_CLASS))
+
+        label_name = re.sub(r'[^a-z0-9]+', ' ', dir_name.lower())
+        training_images = []
+        testing_images = []
+        validation_images = []
+        for file_name in file_list: #For each file in the list
+            base_name = os.path.basename(file_name) #take final name ie filename
+
+            # We want to ignore anything after '_nohash_' in the file name when
+            # deciding which set to put an image in, the data set creator has a way of
+            # grouping photos that are close variations of each other. For example
+            # this is used in the plant disease data set to group multiple pictures of
+            # the same leaf.
+            hash_name = re.sub(r'_nohash_.*$', '', file_name) #Ignore after nohash
+
+            # This looks a bit magical, but we need to decide whether this file should
+            # go into the training, testing, or validation sets, and we want to keep
+            # existing files in the same set even if more files are subsequently
+            # added.
+            # To do that, we need a stable way of deciding based on just the file name
+            # itself, so we do a hash of that and then use that to generate a
+            # probability value that we use to assign it.
+            hash_name_hashed = hashlib.sha1(compat.as_bytes(hash_name)).hexdigest()
+            percentage_hash = ((int(hash_name_hashed, 16) %
+                                (MAX_NUM_IMAGES_PER_CLASS + 1)) *
+                                (100.0 / MAX_NUM_IMAGES_PER_CLASS))
+
+            if percentage_hash < validation_percentage:
+                validation_images.append(base_name)
+            elif percentage_hash < (testing_percentage + validation_percentage):
+                testing_images.append(base_name)
+            else:
+                training_images.append(base_name)
+        result[label_name] = {
+            'dir': dir_name,
+            'training': training_images,
+            'testing': testing_images,
+            'validation': validation_images,
+        }
+    return result #return list for dir name with train,test,validation
+
 def main(_):
     # Needed to make sure the logging output is visible
     tf.logging.set_verbosity(tf.logging.INFO)
@@ -143,10 +241,12 @@ def main(_):
 
     # Set up the pre-trained graph.
     maybe_download_and_extract(model_info['data_url'])
-
     graph, bottleneck_tensor, resized_image_tensor = (
         create_model_graph(model_info))
 
+    # Look at the folder structure, and create lists of all the images.
+    image_lists = create_image_lists(FLAGS.image_dir, FLAGS.testing_percentage,
+                                    FLAGS.validation_percentage)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

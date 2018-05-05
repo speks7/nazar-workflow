d459c2d6ff7b1ddab3a42ecc0dd765bc31858da6
Sunim Acharya <sunim.54@gmail.com>
2018-05-05 21:09:14 +0545

    Add final training operation with setup for tensorflow visualization

--- 02_Image Classification/02a_Class Model/04_Understanding Inception/retrain.py
+++ 02_Image Classification/02a_Class Model/04_Understanding Inception/retrain.py
@@ -572,6 +572,87 @@ def cache_bottlenecks(sess, image_lists, image_dir, bottleneck_dir,
                     tf.logging.info(
                         str(how_many_bottlenecks) + ' bottleneck files created.')
 
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
+def add_final_training_ops(class_count, final_tensor_name, bottleneck_tensor,
+                           bottleneck_tensor_size):
+    """Adds a new softmax and fully-connected layer for training.
+
+    We need to retrain the top layer to identify our new classes, so this function
+    adds the right operations to the graph, along with some variables to hold the
+    weights, and then sets up all the gradients for the backward pass.
+
+    The set up for the softmax and fully-connected layers is based on:
+    https://www.tensorflow.org/versions/master/tutorials/mnist/beginners/index.html
+
+    Args:
+        class_count: Integer of how many categories of things we're trying to
+        recognize.
+        final_tensor_name: Name string for the new final node that produces results.
+        bottleneck_tensor: The output of the main CNN graph.
+        bottleneck_tensor_size: How many entries in the bottleneck vector.
+
+    Returns:
+        The tensors for the training and cross entropy results, and tensors for the
+        bottleneck input and ground truth input.
+    """
+    with tf.name_scope('input'):
+        bottleneck_input = tf.placeholder_with_default(
+            bottleneck_tensor, #Actual graph vlaue from download
+            shape=[None, bottleneck_tensor_size],
+            name='BottleneckInputPlaceholder')
+
+        ground_truth_input = tf.placeholder(tf.float32,
+                                            [None, class_count],
+                                            name='GroundTruthInput') #The ground Truth value
+
+    # Organizing the following ops as `final_training_ops` so they're easier
+    # to see in TensorBoard
+    layer_name = 'final_training_ops'
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            initial_value = tf.truncated_normal(
+                [bottleneck_tensor_size, class_count], stddev=0.001) #From model
+
+            layer_weights = tf.Variable(initial_value, name='final_weights')
+
+            variable_summaries(layer_weights)
+
+        with tf.name_scope('biases'):
+            layer_biases = tf.Variable(tf.zeros([class_count]), name='final_biases')
+            variable_summaries(layer_biases)
+
+        with tf.name_scope('Wx_plus_b'):
+            logits = tf.matmul(bottleneck_input, layer_weights) + layer_biases
+            tf.summary.histogram('pre_activations', logits)
+
+    final_tensor = tf.nn.softmax(logits, name=final_tensor_name)
+    tf.summary.histogram('activations', final_tensor)
+
+    with tf.name_scope('cross_entropy'):
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+            labels=ground_truth_input, logits=logits)
+        with tf.name_scope('total'):
+            cross_entropy_mean = tf.reduce_mean(cross_entropy)
+    tf.summary.scalar('cross_entropy', cross_entropy_mean)
+
+    with tf.name_scope('train'):
+        optimizer = tf.train.GradientDescentOptimizer(FLAGS.learning_rate)
+        train_step = optimizer.minimize(cross_entropy_mean)
+
+    return (train_step, cross_entropy_mean, bottleneck_input, ground_truth_input,
+            final_tensor)
+
 def main(_):
     # Needed to make sure the logging output is visible
     tf.logging.set_verbosity(tf.logging.INFO)
@@ -631,6 +712,12 @@ def main(_):
                 FLAGS.bottleneck_dir, jpeg_data_tensor,
                 decoded_image_tensor, resized_image_tensor,
                 bottleneck_tensor, FLAGS.architecture)
+
+        # Add the new layer that we'll be training.
+        (train_step, cross_entropy, bottleneck_input, ground_truth_input,
+        final_tensor) = add_final_training_ops(
+            len(image_lists.keys()), FLAGS.final_tensor_name, bottleneck_tensor,
+            model_info['bottleneck_tensor_size']) # Add the layer for softmax
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

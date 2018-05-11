f9a6d27f85940fc972fa52c98d534462561eb9fa
Sunim Acharya <sunim.54@gmail.com>
2018-05-11 09:18:17 +0545

    Multiple Image labeling

--- 02_Image Classification/02a_Class Model_Inception/04_Understanding Inception/scripts/label_multiple_images_V2.py
+++ 02_Image Classification/02a_Class Model_Inception/04_Understanding Inception/scripts/label_multiple_images_V2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb  6 14:24:27 2018
+Created on Tue Feb  6 16:30:40 2018
 
 @author: TWang
 
@@ -30,28 +30,14 @@ def load_graph(model_file):
 
 def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
 				input_mean=0, input_std=255):
-  input_name = "file_reader"
-  output_name = "normalized"
-  file_reader = tf.read_file(file_name, input_name)
-  if file_name.endswith(".png"):
-    image_reader = tf.image.decode_png(file_reader, channels = 3,
-                                       name='png_reader')
-  elif file_name.endswith(".gif"):
-    image_reader = tf.squeeze(tf.image.decode_gif(file_reader,
-                                                  name='gif_reader'))
-  elif file_name.endswith(".bmp"):
-    image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
-  else:
-    image_reader = tf.image.decode_jpeg(file_reader, channels = 3,
-                                        name='jpeg_reader')
-  float_caster = tf.cast(image_reader, tf.float32)
-  dims_expander = tf.expand_dims(float_caster, 0);
-  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
-  result = sess.run(normalized)
-  print("result", result)
-  return result
+    
+  img = cv2.imread(file_name) 
+  resized = cv2.resize(img,(input_height, input_width))
+  float_img= np.asfarray(resized,dtype='float32')
+  image_np_expanded = np.expand_dims(float_img, axis=0)
+  normalized = np.divide(np.subtract(image_np_expanded,[input_mean]),[input_std])
+  
+  return normalized
 
 def load_labels(label_file):
   label = []
@@ -92,7 +78,6 @@ if __name__ == "__main__":
       output_name = "import/" + output_layer
       input_operation = graph.get_operation_by_name(input_name);
       output_operation = graph.get_operation_by_name(output_name);
-      print("output",output_operation)
       
       with tf.Session(graph=graph) as sess:
         start = time.time()
